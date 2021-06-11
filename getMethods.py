84e1a3f43779afa9c4bb1e1bea096d5bf9b9f171
Artyom Kozlov <artyom.ch.ch@gmail.com>
2021-06-12 01:59:23 +0300

    external/internal calls

--- getMethods.py
+++ getMethods.py
@@ -1,4 +1,13 @@
 from androguard import misc
+from androguard.core.analysis.analysis import ExternalMethod
+import matplotlib.pyplot as plt
+import networkx as nx
+from androguard.misc import AnalyzeAPK
+from androguard import session
+from androguard.core.analysis.analysis import ExternalMethod
+import matplotlib.pyplot as plt
+import networkx as nx
+
 
 
 def certificate(a):  # CERTIFICATE
@@ -58,10 +67,69 @@ def methods(dx):
             file.write("    calling -> {} -- {} -- descriptor:  {}".format(call.class_name, call.name, call.get_descriptor()) + '\n')
     file.close()
 
+def graph():
+    CFG = nx.DiGraph()
+
+    for m in dx.find_methods(classname="Lcom/google/firebase/FirebaseApp;"):
+        orig_method = m.get_method()
+        print("Found Method --> {}".format(orig_method))
+        # orig_method might be a ExternalMethod too...
+        # so you can check it here also:
+        if isinstance(orig_method, ExternalMethod):
+            is_this_external = True
+            # If this class is external, there will be very likely
+            # no xref_to stored! If there is, it is probably a bug in androguard...
+        else:
+            is_this_external = False
+
+        CFG.add_node(orig_method, external=is_this_external)
+
+        for other_class, callee, offset in m.get_xref_to():
+            if isinstance(callee, ExternalMethod):
+                is_external = True
+            else:
+                is_external = False
+
+            if callee not in CFG.node:
+                CFG.add_node(callee, external=is_external)
+
+            # As this is a DiGraph and we are not interested in duplicate edges,
+            # check if the edge is already in the edge set.
+            # If you need all calls, you probably want to check out MultiDiGraph
+            if not CFG.has_edge(orig_method, callee):
+                CFG.add_edge(orig_method, callee)
+
+    pos = nx.spring_layout(CFG)
+
+    internal = []
+    external = []
+
+    for n in CFG.node:
+        if isinstance(n, ExternalMethod):
+            external.append(n)
+        else:
+            internal.append(n)
+
+    nx.draw_networkx_nodes(CFG, pos=pos, node_color='r', nodelist=internal) # внутренний вызов метода
+    nx.draw_networkx_nodes(CFG, pos=pos, node_color='b', nodelist=external) # внешний вызов метода
+    nx.draw_networkx_edges(CFG, pos=pos, arrow=True)
+
+
+    nx.draw_networkx_labels(CFG, pos=pos, labels={x: "{} {}".format(x.get_class_name(), x.get_name()) for x in CFG.adj})
+
+    plt.draw()
+    plt.show()
+
 
 
 
 a, d, dx = misc.AnalyzeAPK("malware.apk")
+
+
+
+
+
+
 certificate(a)
 print("Certificate save")
 permission(a)
@@ -74,5 +142,6 @@ classes(dx)
 print("Classes save")
 methods(dx)
 print("Methods save")
+graph()
 
 
